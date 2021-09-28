d62ebdd5b4b93201d7de1b62c91a02f5f5aba87c
AIIT-USTC-0835 <91528136+AIIT-USTC-0835@users.noreply.github.com>
2021-09-28 16:41:19 +0800

    Add files via upload

---
+++ 001——第一章 绪论/1.3—抽象数据类型的表示与实现/1.Dev-C++/合——三元组Triplet/合——三元组Triplet/c1-1.h
@@ -0,0 +1,9 @@
+#include "c1.h"
+//三元组抽象数据类型Triplet 
+//c1-1.h 采用动态分配的顺序存储结构
+
+typedef int ElemType;//定义抽象数据类型ElemType,在本程序中为整型
+//typedef double ElemType;//定义抽象数据类型ElemType,在本程序中为双精度型
+
+typedef ElemType *Triplet;//由InitTriplet分配3个元素的存储空间
+//Triplet类型是ElemType类型的指针，存放ElemType类型的地址 
